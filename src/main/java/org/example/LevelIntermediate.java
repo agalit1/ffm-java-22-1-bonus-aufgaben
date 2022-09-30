154b45c6e283be138d775bf01cd53ecd145adbff
anagalit <ana.galit1@gmail.com>
2022-09-30 15:59:17 +0200

    Add Level Intermediate task

---
+++ src/main/java/org/example/LevelIntermediate.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class LevelIntermediate {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String numberReturnString(int number) {
+        String[] numberString = new String[]{"Eins", "Zwei", "Drei", "Vier", "Fuenf", "Sechs", "Sieben", "Acht", "Neun", "Zehn"};
+        return numberString[number -1];
+
+    }
+}
---
+++ src/test/java/LevelIntermediateTest.java
@@ -0,0 +1,21 @@
+import org.example.LevelIntermediate;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelIntermediateTest {
+
+    @Test
+    public void fiveReturnsFuenf() {
+        //given
+        int number = 5;
+
+        //when
+        String actual = LevelIntermediate.numberReturnString(number);
+
+        //then
+        assertEquals("Fuenf", actual);
+
+    }
+
+}
