f6c3adba107eed141953569654bc4236ebb27f76
anagalit <ana.galit1@gmail.com>
2022-09-30 14:37:09 +0200

    Add classes

---
+++ src/main/java/exercise1/LevelNoviceTask.java
@@ -0,0 +1,16 @@
+package LevelNoviceTask;
+
+public class LevelNovice {
+    public static void main(String[] args) {
+
+//        Scanner scan = new Scanner(System.in);
+//
+//        System.out.println("Enter your name: ");
+//        String name = scan.nextLine();
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(name);
+//        }
+
+
+    }
+}
---
+++ src/test/java/LevelNoviceTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelNoviceTest {
+
+    @Test
+    public void inputEqualsOutput() {
+        //given
+        String name = "ana";
+
+        //when
+
+
+        //then
+
+
+    }
+
+
+}
